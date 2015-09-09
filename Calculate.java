1b7d37c3180e674007016978890f53f013549e97
unknown <ymalik@MA14951.add0.masergy.com>
2015-09-09 10:27:52 -0500

    no message

---
+++ Calculate.java
@@ -0,0 +1,18 @@
+public class Calculate {
+
+	public double add(double x, double y) {
+		return x + y;
+	}
+	
+	public double sub(double x, double y) {
+		return x - y;
+	}
+	
+	public double mult(double x, double y) {
+		return x * y;
+	}
+	
+	public double div(double x, double y) {
+		return x / y;
+	}
+}
